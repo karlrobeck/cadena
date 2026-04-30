768d5817423bfde93b79fb3005a574be51286531
karl robeck alferez <karlalferezfx@gmail.com>
2026-04-30 20:26:21 +0800

    Add libp2p dependencies and implement basic peer ID generation and listening
    - Updated Cargo.toml to include dependencies for anyhow, libp2p, and tokio.
    - Modified main.rs to generate a local peer ID and set up a libp2p swarm with TCP, Noise, and Yamux configurations.
    - Implemented a listening address for the swarm.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
-fn main() {
-    println!("Hello, world!");
+use std::time::Duration;
+
+use libp2p::{PeerId, identity};
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    let local_key = identity::Keypair::generate_ed25519();
+
+    let local_peer_id = PeerId::from(local_key.public());
+
+    println!("Local node PeerID: {:?}", local_peer_id);
+
+    let mut swarm = libp2p::SwarmBuilder::with_existing_identity(local_key)
+        .with_tokio()
+        .with_tcp(
+            libp2p::tcp::Config::default(),
+            libp2p::noise::Config::new,
+            libp2p::yamux::Config::default,
+        )?
+        .with_behaviour(|_| libp2p::swarm::dummy::Behaviour)?
+        .with_swarm_config(|c| c.with_idle_connection_timeout(Duration::from_secs(60)))
+        .build();
+
+    swarm.listen_on("/ip4/0.0.0.0/tcp/0".parse()?)?;
+
+    Ok(())
 }
